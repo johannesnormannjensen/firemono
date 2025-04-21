c7d5bd6780d758b6829f7f6c674458066659ba13
Johannes Normann Jensen <johannes@nestech.dk>
2025-04-21 23:54:36 +0200

    id name

--- apps/demo/angular/src/app/nx-welcome.component.ts
+++ apps/demo/angular/src/app/nx-welcome.component.ts
@@ -1,14 +1,40 @@
 import { CommonModule, JsonPipe } from '@angular/common';
 import { Component, inject, ViewEncapsulation } from '@angular/core';
 import { EnvironmentService } from '@firemono/demo/angular/environment';
-import { Firestore, collectionData, collection } from '@angular/fire/firestore';
+import { Firestore, collectionData, collection, DocumentData, FirestoreDataConverter, QueryDocumentSnapshot, SnapshotOptions } from '@angular/fire/firestore';
+import { User } from '@firemono/demo/data';
+
+const converter: FirestoreDataConverter<User> = {
+  toFirestore(user: User): DocumentData {
+    return {
+      ...user,
+    };
+  },
+  fromFirestore(
+    snapshot: QueryDocumentSnapshot,
+    options: SnapshotOptions
+  ): User {
+    const data = snapshot.data(options)!;
+    return {
+      id: snapshot.id,
+      name: data['name'],
+      lastUpdate: data['lastUpdate'],
+    };
+  }
+};
 
 @Component({
   selector: 'demo-nx-welcome',
-  imports: [CommonModule, JsonPipe],
+  imports: [CommonModule],
   template: `
     <h1>Welcome to Firemono!</h1>
     <h2>Angular Demo</h2>
+    @for(user of users$ | async; track user) {
+      <div>
+        <p>Id: {{ user.id }}</p>
+        <h3>Name: {{ user.name }}</h3>
+      </div>
+    }
   `,
   styles: [],
   encapsulation: ViewEncapsulation.None,
@@ -17,6 +43,5 @@ export class NxWelcomeComponent {
   apiUrl = inject(EnvironmentService).apiUrl;
 
   firestore = inject(Firestore);
-  itemCollection = collection(this.firestore, 'items');
-  item$ = collectionData(this.itemCollection);
+  users$ = collectionData<User>(collection(this.firestore, 'user').withConverter(converter));
 }
--- libs/demo/data/src/lib/user.ts
+++ libs/demo/data/src/lib/user.ts
@@ -1,6 +1,7 @@
 import { Timestamp } from 'firebase-admin/firestore';
 
 export interface User {
+    id: string;
     name: string;
     lastUpdate: Timestamp;
 }
